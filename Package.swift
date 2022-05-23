af4fd296282886d82423aaaddef957ca40249a51
Midori Minato <midori.minato.99@gmail.com>
2022-05-23 21:09:43 +0900

    Changed to Swift Package

---
+++ Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version: 5.6
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "NMSSH",
+    products: [
+        .library(
+            name: "NMSSH",
+            targets: ["NMSSH"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(
+            name: "NMSSH"),
+    ]
+)
